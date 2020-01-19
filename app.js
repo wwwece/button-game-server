24b1f23c2309075eb919e57167597f3284904fff
WeceW <toni.weckroth@gmail.com>
2020-01-19 19:20:21 +0200

    Added CORS support

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   graphiql: true
